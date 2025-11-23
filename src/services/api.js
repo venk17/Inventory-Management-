a6c6baa59aa6c7a4cec92c404632bc2c2c2d10f8
venk17 <k.venkatramanan2002@gamil.com>
2025-11-23 14:31:24 +0530

    update backen url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://inventory-management-2ku2.onrender.com/api';
 
 class ApiService {
   async request(url, options = {}) {
